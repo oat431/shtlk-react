e82fff50c7a744a29b49496be421aa8cdebb1545
oat431 <oat431@gmail.com>
2026-02-22 22:53:15 +0700

    update .env var prefix

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import NavBar from "./components/NavBar.tsx";
 import Footer from "./components/Footer.tsx";
 
 function App() {
-    const env = import.meta.env.VITE_CHECK
+    const env = import.meta.env.SHTLK_CHECK
     const url = import.meta.env.BASE_URL
-    const api = import.meta.env.VITE_BASE_API
+    const api = import.meta.env.SHTLK_BASE_API
 
   return (
       <>
           <NavBar />
-          {/*<div className="skeleton h-full w-full"></div>*/}
-          <h1>{env}</h1>
-          <h1>{url}</h1>
-          <h1>{api}</h1>
+          <div className="skeleton h-full w-full">
+              <h1>{env}</h1>
+              <h1>{url}</h1>
+              <h1>{api}</h1>
+          </div>
           <Footer />
       </>
   )
