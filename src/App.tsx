afe5e9450006b09f2064554dd7d0b555c21bed2b
oat431 <oat431@gmail.com>
2026-02-22 22:42:29 +0700

    add: env for development

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import NavBar from "./components/NavBar.tsx";
 import Footer from "./components/Footer.tsx";
 
 function App() {
+    const env = import.meta.env.VITE_CHECK
+    const url = import.meta.env.BASE_URL
+    const api = import.meta.env.VITE_BASE_API
 
   return (
       <>
           <NavBar />
-          <div className="skeleton h-screen w-screen"></div>
+          {/*<div className="skeleton h-full w-full"></div>*/}
+          <h1>{env}</h1>
+          <h1>{url}</h1>
+          <h1>{api}</h1>
           <Footer />
       </>
   )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,3 @@
-// <template>
-//     <footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-//         <aside>
-//             <p>Copyright © 2026 - All right reserved by Sahachan Tippimwong</p>
-//         </aside>
-//     </footer>
-// </template>
-
 function Footer(){
     return (
         <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
